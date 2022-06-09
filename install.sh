e8c42a1c7819cb3ac76ee4cfa72fc68197778a66
sophie <zvava@disroot.org>
2022-06-09 18:07:04 +0100

    finish install script for regular linux

--- install.sh
+++ install.sh
@@ -8,19 +8,25 @@
 
 [ $(whoami) = "root" ] || { echo "[aborting] not running as root" ; exit ; }
 
+# cd to location of script
+cd "${0%/*}"
+
 printf "%s\n%s" \
 	"-> about to install dotfiles from mininmobile/config" \
 	"-> enter the username of your main account: "
 read -r installuser
+
 printf "%s" \
 	"-> is \"$installuser\" correct? (y/n) "
 read -r ans
 [ "$ans" = y ] || { echo "[aborting]" ; exit ; }
 
-cp -rp .config /home/$installuser/
-cp -rp .scripts /home/$installuser/
-cp -rp .nano /home/$installuser/
-# TODO: do this and update the username
-cp environment /etc/environment
-# TODO: make this with correct perms
-# mkdir /home/$installuser/.nano.backups
+cp -rp .config .scripts .bash* /home/$installuser/
+
+# nano configs
+cp -rp .nano .nanorc /home/$installuser/
+mkdir /home/$installuser/.nano.backups
+chown $installuser.$installuser /home/$installuser/.nano.backups
+
+# add .scripts to PATH
+echo "PATH=/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/home/$installuser/.scripts" > /etc/environment
